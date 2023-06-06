a13a35d28174288e7edf9eae1eaf4bac623083dd
andrew-s-taylor <andrew@tinymice.co.uk>
2023-06-06 09:17:35 +0100

    New script

---
+++ Chapter-5/assign-google-play-app.ps1
@@ -0,0 +1,30 @@
+$appname = "Microsoft Authenticator"
+
+$appid = ((Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/beta/deviceAppManagement/mobileApps" -Method Get -ContentType "application/json" -OutputType PSObject).value | Where-Object {$_.'@odata.type' -eq '#microsoft.graph.iosVppApp'} | Where-Object {$_.displayName -eq $appname}).id
+
+$url = "https://graph.microsoft.com/beta/deviceAppManagement/mobileApps/$appid/assign"
+
+$json = @"
+{
+	"mobileAppAssignments": [
+		{
+			"@odata.type": "#microsoft.graph.mobileAppAssignment",
+			"intent": "Required",
+			"settings": {
+				"@odata.type": "#microsoft.graph.iosVppAppAssignmentSettings",
+				"isRemovable": false,
+				"preventAutoAppUpdate": false,
+				"preventManagedAppBackup": true,
+				"uninstallOnDeviceRemoval": true,
+				"useDeviceLicensing": true,
+				"vpnConfigurationId": null
+			},
+			"target": {
+				"@odata.type": "#microsoft.graph.allDevicesAssignmentTarget"
+			}
+		}
+	]
+}
+"@
+
+Invoke-MgGraphRequest -Url $url -Method Post -Body $json -ContentType "application/json"
