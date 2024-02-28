0f7ba2b8c6dd27bc0f87d29bd1c07cf13b880d34
andrew-s-taylor <andrew@tinymice.co.uk>
2024-02-28 14:56:54 +0000

    Filter fix

--- Chapter1/disable-delete-devices.ps1
+++ Chapter1/disable-delete-devices.ps1
@@ -8,20 +8,22 @@ $daystoremove = 120
 
 # Disable devices that have not logged on within the specified number of days
 $dt = (Get-Date).AddDays(-$daystodisable)
-$Devices = Get-MgDevice -All | Where-Object {$_.ApproximateLastLogonTimeStamp -le $dt}
+$Devices = Get-MgDevice -All -Filter "ApproximateLastSignInDateTime le $($dt.ToString("yyyy-MM-dd"))T00:00:00z"
 foreach ($Device in $Devices) {
     $deviceid = $Device.Id
     Write-Host "Disabling device with ID: $deviceid"
-    Update-MgDevice -DeviceId $deviceid -AccountEnabled $false
+    Update-MgDevice -DeviceId $deviceid -AccountEnabled:$false
     Write-Host "Device disabled successfully"
 }
 
+
 # Delete disabled devices that have not logged on within the specified number of days
 $dt = (Get-Date).AddDays(-$daystoremove)
-$Devices = Get-MgDevice -All | Where-Object {($_.ApproximateLastLogonTimeStamp -le $dt) -and ($_.AccountEnabled -eq $false)}
+$Devices = Get-MgDevice -All -Filter "ApproximateLastSignInDateTime le $($dt.ToString("yyyy-MM-dd"))T00:00:00z" | Where-Object {($_.AccountEnabled -eq $false)}
 foreach ($Device in $Devices) {
     $deviceid = $Device.Id
     Write-Host "Removing device with ID: $deviceid"
     Remove-MgDevice -DeviceId $deviceid
     Write-Host "Device removed successfully"
 }
+
